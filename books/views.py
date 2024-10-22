86432ffb0247084bae6caa33993307fe7b5009e5
fatix07 <fatimazahra_drissi3@um5.ac.ma>
2024-10-22 15:42:01 +0100

    Adding view/urls/templates for listing books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.views.generic import ListView
+class BookListView(ListView):
+     model=Book
+     template_name="book_list.html"
 # Create your views here.
