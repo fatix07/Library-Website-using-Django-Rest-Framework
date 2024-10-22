bad581d37f6dc4054409f30db25ddedaaa764afd
fatix07 <fatimazahra_drissi3@um5.ac.ma>
2024-10-22 16:13:18 +0100

    Adding Create book crud

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BookListView
+from .views import BookListView,BookCreateView
 urlpatterns = [
     path('',BookListView.as_view(),name="home"),
+    path('create/',BookCreateView.as_view(),name="create"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 class BookListView(ListView):
      model=Book
      template_name="book_list.html"
+class BookCreateView(CreateView):
+    model=Book
+    template_name='books/book_form.html'
+    fields=['title','subtitle','author','isbn']
+    success_url='/'
+
 # Create your views here.
